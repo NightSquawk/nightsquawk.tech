c90cb8b1b677be8af8223656dcc16af3ac90389d
Kevin <39208150+Kvrnn@users.noreply.github.com>
2024-06-25 23:09:43 -0700

    Update gtag.js

--- static/js/gtag.js
+++ static/js/gtag.js
@@ -1,4 +1,16 @@
-// Load the Google Tag Manager script
+// Google Tag Manager
+(function(w,d,s,l,i){
+    w[l]=w[l]||[];
+    w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-MXF4B9NG');
+
+// Load the Google Analytics script
 (function() {
     var gtagScript = document.createElement('script');
     gtagScript.async = true;
@@ -13,4 +25,17 @@
         gtag('js', new Date());
         gtag('config', 'G-LH4XY2QF0M');
     };
+})();
+
+// Google Tag Manager (noscript)
+(function() {
+    var noscript = document.createElement('noscript');
+    var iframe = document.createElement('iframe');
+    iframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-MXF4B9NG';
+    iframe.height = '0';
+    iframe.width = '0';
+    iframe.style.display = 'none';
+    iframe.style.visibility = 'hidden';
+    noscript.appendChild(iframe);
+    document.body.appendChild(noscript);
 })();
